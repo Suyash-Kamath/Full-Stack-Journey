1e6306392e550da07f0e6c1dfb653facbb701e86
Suyash-Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2024-08-13 23:15:18 +0530

    Create index.js

---
+++ NodeJS-By-Piyush-Garg/3.Hello-World-NodeJS/index.js
@@ -0,0 +1,21 @@
+console.log('Hey there I am JavaScript')
+
+// console.log(window); //Error milega stating window is not defined , browser me kiya toh windows object show hoga
+
+// console.log(alert('Hey')); //same problem , alert is not defined
+
+/* 
+chrome ke V8 engine ko directly embed nahi kiya hai C++ ke saath, jitne bhi DOM or window ke 
+related element hai usko nikaal diya hai , ,Jo DOM or window ke objects availabale nahi hai , isme 
+core functionalities 
+hai which we needed for server side
+
+Concepts like bcrypt , File Handling packages , etc. are added here
+
+NPM file banata hai jo project banana me kaam aati hai 
+tumhara npm package.json hi padega jabhi git clonoe karenge toh and then install the modules
+
+package.json file is a configuration
+
+*/
+
