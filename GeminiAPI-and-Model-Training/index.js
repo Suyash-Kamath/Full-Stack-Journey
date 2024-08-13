907dda9519a2dc67d5ff7ed8e26dbcba40282968
Suyash-Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2024-08-13 23:10:10 +0530

    Create index.js

---
+++ GeminiAPI-and-Model-Training/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./router');
+require('dotenv/config');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000
+
+require('./geminiAPI')
+app.use('/api',router)
+
+app.listen(port,()=>{
+    console.log(`App running on port ${port}`);
+    
+})
